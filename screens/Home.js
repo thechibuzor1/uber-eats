f896c5110f8edc103f081d25b0743fa4075c11b3
Chibuzor <chibuzorm26@example.com>
2022-05-15 16:32:43 +0100

    search bar

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,22 @@
-import { View, Text, SafeAreaView,StyleSheet,StatusBar } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, StatusBar } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <HeaderTabs />
-    </SafeAreaView >
+      <View style={{backgroundColor: "white", padding: 15}}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop:StatusBar.currentHeight
-    }
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+    backgroundColor: "#eee",
+  },
 });
