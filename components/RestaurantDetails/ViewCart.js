4c44819be011f85ecf6d86b597e84d5c03ab74d5
Chibuzor <chibuzorm26@example.com>
2022-05-21 14:13:19 +0100

    view cart modal

--- components/RestaurantDetails/ViewCart.js
+++ components/RestaurantDetails/ViewCart.js
@@ -1,9 +1,14 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from "react-native";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import OrderItem from "./OrderItem";
 
 export default function ViewCart() {
-  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const { items, restaurantName } = useSelector(
+    (state) => state.cartReducer.selectedItems
+  );
   const total = items
     .map((item) => Number(item.price.replace("$", "")))
     .reduce((prev, curr) => prev + curr, 0);
@@ -12,9 +17,92 @@ export default function ViewCart() {
     style: "currency",
     currency: "USD",
   });
-  console.log(totalUSD);
+
+  const styles = StyleSheet.create({
+    modalContainer: {
+      flex: 1,
+      justifyContent: "flex-end",
+      backgroundColor: "rgba(0,0,0,0.7)",
+    },
+    modalCheckoutContainer: {
+      backgroundColor: "white",
+      padding: 16,
+      height: 500,
+      borderWidth: 1,
+    },
+    restaurantName: {
+      textAlign: "center",
+      fontWeight: "600",
+      fontSize: 18,
+      marginBottom: 10,
+    },
+    subtotalContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 15,
+    },
+    subtotalText: {
+      textAlign: "left",
+      fontWeight: "600",
+      fontSize: 15,
+      marginBottom: 10,
+    },
+  });
+
+  const checkoutModalContent = () => {
+    return (
+      <>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalCheckoutContainer}>
+            <Text style={styles.restaurantName}>{restaurantName}</Text>
+            {items.map((item, index) => (
+              <OrderItem key={index} item={item} />
+            ))}
+            <View style={styles.subtotalContainer}>
+              <Text style={styles.subtotalText}>Subtotal</Text>
+              <Text> ${Math.round(totalUSD * 100) / 100}</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <TouchableOpacity style={{
+                marinTop: 20,
+                backgroundColor: 'black',
+                alignItems: 'center',
+                padding: 13,
+                borderRadius: 30,
+                width: 300,
+                position: 'relative',
+              }}
+              onPress={() => setModalVisible(false)}>
+                <Text style={{color: 'white', fontSize: 20}}>Checkout</Text>
+                <Text style={{
+                  position: 'absolute',
+                  right: 20,
+                  color: 'white',
+                  fontSize: 15,
+                  top: 17
+                }}>{total ? '$' + Math.round(totalUSD * 100) / 100 : ""}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </>
+    );
+  };
   return (
     <>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        transparent={true}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        {checkoutModalContent()}
+      </Modal>
       {total ? (
         <View
           style={{
@@ -36,7 +124,6 @@ export default function ViewCart() {
           >
             <TouchableOpacity
               style={{
-                
                 backgroundColor: "black",
                 flexDirection: "row",
                 justifyContent: "flex-end",
@@ -46,11 +133,14 @@ export default function ViewCart() {
                 width: 300,
                 position: "relative",
               }}
+              onPress={() => setModalVisible(true)}
             >
               <Text style={{ color: "white", fontSize: 20, marginRight: 50 }}>
                 View Cart
               </Text>
-              <Text style={{ color: "white", fontSize: 20 }}>${Math.round(totalUSD * 100)/100}</Text>
+              <Text style={{ color: "white", fontSize: 20 }}>
+                ${Math.round(totalUSD * 100) / 100}
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
