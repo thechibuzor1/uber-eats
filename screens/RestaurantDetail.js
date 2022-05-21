afc4ac4e868655b3c217e5cf4c8924fa3d9ea5fa
Chibuzor <chibuzorm26@example.com>
2022-05-21 05:39:18 +0100

    view cart

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,14 +3,17 @@ import React from "react";
 import { Divider } from "react-native-elements";
 import About from "../components/RestaurantDetails/About";
 import MenuItems from "../components/RestaurantDetails/MenuItems";
+import ViewCart from "../components/RestaurantDetails/ViewCart";
 
-export default function RestaurantDetail({route}) {
+export default function RestaurantDetail({ route, navigation }) {
   return (
-      <>
-      <About route={route}/>
-      <Divider width={1.8} style={{marginVertical: 20}} />
+    <>
+      <About route={route} />
+      <Divider width={1.8} style={{ marginVertical: 20 }} />
       <ScrollView showsVerticalScrollIndicator={false}>
-        <MenuItems/>
-    </ScrollView></>
+        <MenuItems />
+      </ScrollView>
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
+    </>
   );
 }
